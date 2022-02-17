173c6ffde9fbcd5a5f680c32272a0d01eb25f0f2
Zak <zak.johansson@gmail.com>
2022-02-16 16:10:44 -0800

    operator function work

--- assets/script.js
+++ assets/script.js
@@ -1,42 +1,36 @@
 const displayMe = document.querySelector(".display");
-const showMe = document.querySelector('.upperDisplay');
+const showMe =    document.querySelector('.upperDisplay');
 
 const numbers = {
-one: document.querySelector('.one'),
-two: document.querySelector('.two'),
-three: document.querySelector('.three'),
-four: document.querySelector('.four'),
-five: document.querySelector('.five'),
-six: document.querySelector('.six'),
-seven: document.querySelector('.seven'),
-eight: document.querySelector('.eight'),
-nine: document.querySelector('.nine'),
-zero: document.querySelector('.zero'),
+    one:    document.querySelector('.one'),
+    two:    document.querySelector('.two'),
+    three:  document.querySelector('.three'),
+    four:   document.querySelector('.four'),
+    five:   document.querySelector('.five'),
+    six:    document.querySelector('.six'),
+    seven:  document.querySelector('.seven'),
+    eight:  document.querySelector('.eight'),
+    nine:   document.querySelector('.nine'),
+    zero:   document.querySelector('.zero'),
 };
 
 const operators = {
-    divide: document.querySelector('.divide'),
-    add: document.querySelector('.add'),
-    subtract: document.querySelector('.subtract'),
-    multiply: document.querySelector('.multiply'),
-   /* decimal: document.querySelector('.decimal'),
-    equal: document.querySelector('.equal'),
-    power: document.querySelector('.power'), */
-    clear: document.querySelector('.clear'),
+    divide:     document.querySelector('.divide'),
+    add:        document.querySelector('.add'),
+    subtract:   document.querySelector('.subtract'),
+    multiply:   document.querySelector('.multiply'),
+  /*decimal:    document.querySelector('.decimal'),
+    equal:      document.querySelector('.equal'),
+    power:      document.querySelector('.power'),*/
+    clear:      document.querySelector('.clear'),
 }
 
 displayMe.innerHTML= '';
-
 // Takes the parameter of the button clicked 
 // returns the element into the display
 function displayThis(){
     displayMe.innerHTML=`${this.textContent}`;
     let holdMe = this;
-};
-
-//takes operator and 2 numbers and calls arithmetic function on numbers
-function operate(){
-
 };
 
 numbers.one.addEventListener('click', displayThis);
@@ -52,11 +46,7 @@ numbers.zero.addEventListener('click', displayThis);
 
 operators.add.addEventListener('click', add);
 
-
-
 operators.clear.addEventListener('click', clear => displayMe.innerHTML='0');
-
-
 /*operators.power.addEventListener('click', power => {
     if(displayMe !== '') {
         displayMe.innerHTML='0';
@@ -81,4 +71,7 @@ function divide(firstNumber, secondNumber){
 function multiply(firstNumber, SecondNumber){
     return firstNumber * SecondNumber;
 };
-
+//takes operator and 2 numbers and calls arithmetic function on numbers
+function operate(firstNumber, secondNumber){
+holdMe
+};
