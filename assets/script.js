9d27f7f5341b69cc9cde902af3da9c86a9126e97
Zak <zak.johansson@gmail.com>
2022-02-18 15:06:37 -0800

    hard reset and added up to date code

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,9 @@
 const displayMe = document.querySelector(".display");
 const showMe =    document.querySelector('.upperDisplay');
+let operatorArray = [];
+let answer = [];
+
+displayMe.innerHTML= '';
 
 const numbers = {
     one:    document.querySelector('.one'),
@@ -14,64 +18,72 @@ const numbers = {
     zero:   document.querySelector('.zero'),
 };
 
+const numberEvent = {
+    one:    numbers.one.addEventListener('click', displayThis),
+    two:    numbers.two.addEventListener('click', displayThis),
+    three:  numbers.three.addEventListener('click', displayThis),
+    four:   numbers.four.addEventListener('click', displayThis),
+    five:   numbers.five.addEventListener('click', displayThis),
+    six:    numbers.six.addEventListener('click', displayThis),
+    seven:  numbers.seven.addEventListener('click', displayThis),
+    eight:  numbers.eight.addEventListener('click', displayThis),
+    nine:   numbers.nine.addEventListener('click', displayThis),
+    zero:   numbers.zero.addEventListener('click', displayThis),
+};
+
 const operators = {
     divide:     document.querySelector('.divide'),
     add:        document.querySelector('.add'),
     subtract:   document.querySelector('.subtract'),
     multiply:   document.querySelector('.multiply'),
-  /*decimal:    document.querySelector('.decimal'),
     equal:      document.querySelector('.equal'),
-    power:      document.querySelector('.power'),*/
     clear:      document.querySelector('.clear'),
-}
+};
 
-displayMe.innerHTML= '';
-// Takes the parameter of the button clicked 
-// returns the element into the display
+const operatorFunctions = {
+    add:        add => operatorArray[0] + operatorArray[1],
+    subtract:   subtract => operatorArray[0] - operatorArray[1],
+    divide:     divide => operatorArray[0] / operatorArray[1],
+    multiply:   multiply => operatorArray[0] * operatorArray[1],
+};
+
+const operatorEvent = {
+    add:        operators.add.addEventListener('click', operatorFunctions.add),
+    subtract:   operators.subtract.addEventListener('click', operatorFunctions.subtract),
+    multiply:   operators.multiply.addEventListener('click', operatorFunctions.multiply),
+    divide:     operators.divide.addEventListener('click', operatorFunctions.divide),
+    equal:      operators.equal.addEventListener('click', equals),
+    clear:      operators.clear.addEventListener('click',  
+                    function clear(){
+                        displayMe.innerHTML='0';
+                        operatorArray.length = 0;
+                    }),
+};
+
 function displayThis(){
     displayMe.innerHTML=`${this.textContent}`;
-    let holdMe = this;
+    operatorArray.push(this);
 };
 
-numbers.one.addEventListener('click', displayThis);
-numbers.two.addEventListener('click', displayThis);
-numbers.three.addEventListener('click', displayThis);
-numbers.four.addEventListener('click', displayThis);
-numbers.five.addEventListener('click', displayThis);
-numbers.six.addEventListener('click', displayThis);
-numbers.seven.addEventListener('click', displayThis);
-numbers.eight.addEventListener('click', displayThis);
-numbers.nine.addEventListener('click', displayThis);
-numbers.zero.addEventListener('click', displayThis);
-
-operators.add.addEventListener('click', add);
-
-operators.clear.addEventListener('click', clear => displayMe.innerHTML='0');
-/*operators.power.addEventListener('click', power => {
-    if(displayMe !== '') {
-        displayMe.innerHTML='0';
-    } else {
-        displayMe.innerHTML='';
-    }
-}); */
-
-
-function add(firstNumber, secondNumber){
-    return firstNumber + secondNumber;
+function equals() {
+    displayMe.innerHTML= answer[0];
 };
 
-function subtract(firstNumber, secondNumber){
-    return firstNumber - secondNumber;
+function operator() {
+ if (operatorArray[1] !== null) {
+     let equal = operatorEvent.key;
+ } 
+ answer.push(equal);
 };
 
-function divide(firstNumber, secondNumber){
-    return firstNumber / secondNumber;
-};
+console.log(answer);
 
-function multiply(firstNumber, secondNumber){
-    return firstNumber * secondNumber;
-};
 //takes operator and 2 numbers and calls arithmetic function on numbers
-function operate(firstNumber, secondNumber){
-holdMe
-};
+
+//take first button click and hold as variable
+//take second button click and hold as variable
+// add operator to it
+// if second number isn't inputtted, hold operator until second number is inputted
+// if second number exists, push answer to
+// hold answer in variable
+// use equal to display answer variable.
